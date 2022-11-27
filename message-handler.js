75f425fe954d6fe07f60bd57d32a361d3705bb35
Ray Zheng <zhengrc19@mails.tsinghua.edu.cn>
2022-11-27 17:04:50 +0800

    try escape unicode

--- message-handler.js
+++ message-handler.js
@@ -44,7 +44,7 @@ export default class RecorderHandler {
 
         let str_msgs = JSON.stringify(simple_objects, null, 4); //indentation in json format, human readable
         console.log(str_msgs);
-        let json_url = "data:application/x-mimearchive;base64," + btoa(str_msgs);
+        let json_url = "data:application/x-mimearchive;base64," + btoa(unescape(encodeURIComponent(str_msgs)));
         let json_filename = `record_${date_str}/log/actions.json`;
         chrome.downloads.download({
             filename: json_filename,
@@ -54,7 +54,7 @@ export default class RecorderHandler {
         });
 
         let str_sent = JSON.stringify(this.sended_msgs, null, 4);
-        let send_url = "data:application/x-mimearchive;base64," + btoa(str_sent);
+        let send_url = "data:application/x-mimearchive;base64," + btoa(unescape(encodeURIComponent(str_sent)));
         let send_filename = `record_${date_str}/log/log.json`;
         chrome.downloads.download({
             filename: send_filename,
