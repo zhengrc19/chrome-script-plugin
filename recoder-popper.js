d55294e6d6eb8f820aeeeda02c8a4139a8ca88a8
Ray Zheng <zhengrc19@mails.tsinghua.edu.cn>
2022-11-30 16:42:54 +0800

    fixed textbox cannot enter

--- recoder-popper.js
+++ recoder-popper.js
@@ -832,7 +832,7 @@ function dragElement(elmnt) {
   
     function dragMouseDown(e) {
       e = e || window.event;
-      e.preventDefault();
+    //   e.preventDefault();
       // get the mouse cursor position at startup:
       pos3 = e.clientX;
       pos4 = e.clientY;
