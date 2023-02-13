a5133fbf083903f775bb0a54d5fdf0fc78cb7793
Emily <1207666367@qq.com>
2023-02-13 16:52:07 +0800

    add script for mhtml to html

---
+++ scripts/get_html.py
@@ -0,0 +1,34 @@
+import email
+import os, sys
+import quopri
+
+def main():
+    if len(sys.argv)==1:
+        print("Usage: %s filename" % os.path.basename(sys.argv[0]))
+        sys.exit(1)
+
+    mhtml_path = sys.argv[1]
+    assert mhtml_path[-6:] == ".mhtml"
+    with open(mhtml_path, "r") as mailFile:
+        msg = email.message_from_string(mailFile.read())
+    
+    if msg.is_multipart():
+        for id, part in enumerate(msg.get_payload()):
+            if part["Content-Type"] == "text/html":
+                print(id, part["Content-Location"])
+                fname = os.path.join(os.path.dirname(mhtml_path), os.path.basename(mhtml_path)[:-6] + ".html")
+                html_context = part.get_payload()
+
+                print(part["Content-Transfer-Encoding"])
+                if part["Content-Transfer-Encoding"] == "quoted-printable":
+                    html_context = quopri.decodestring(html_context)
+                else:
+                    raise NotImplementedError()  # 似乎还可能是 base64
+                
+                with open(fname, "wb") as f:
+                    f.write(html_context)
+                
+                break
+
+if __name__=="__main__":
+    main()
